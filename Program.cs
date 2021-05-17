be8b2b336f3a70799049bb8b6807ca6e931944d0
Daniil Doroshenko <43093634+Medniu@users.noreply.github.com>
2021-05-17 10:18:22 +0300

    some new features

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
+using System.Configuration;
+using System.Collections.Specialized;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -9,7 +12,6 @@ namespace StationConsumer
     {
         static void Main(string[] args)
         {
-
             Station station = new Station
             {
                 Id = Guid.NewGuid(),
@@ -19,17 +21,21 @@ namespace StationConsumer
             };
 
             var factory = new ConnectionFactory() {
-                HostName = "20.52.186.170",
-                Port = 5672,
-                VirtualHost = "/",
-                Password = "admin",
-                UserName = "admin"
+                HostName = ConfigurationManager.AppSettings["hostName"],
+                Port = Convert.ToInt32(ConfigurationManager.AppSettings["port"]),
+                VirtualHost = ConfigurationManager.AppSettings["virtualHost"],
+                Password = ConfigurationManager.AppSettings["password"],
+                UserName = ConfigurationManager.AppSettings["userName"]
             };
+
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
+                Thread thread = new Thread(() => Status(connection, station));
+                thread.Start(); // запускаем поток
+
                 string message = $"{station.Id} {station.Latitude} {station.Longitude}";
-                var body = Encoding.UTF8.GetBytes(message);
+                var body = Encoding.UTF8.GetBytes(message.Replace(",", "."));
 
                 channel.BasicPublish(exchange: "InboundExchange",
                                      routingKey: "Registration",
@@ -38,23 +44,38 @@ namespace StationConsumer
 
 
                 channel.QueueDeclare(queue: $"{station.Id}",
-                             durable: true,
-                             exclusive: false,
-                             autoDelete: false,
-                             arguments: null);
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
 
                 channel.QueueBind(queue: $"{station.Id}",
-                        exchange: "OutboundExchange",
-                        routingKey: $"{station.Id}");
+                                  exchange: "OutboundExchange",
+                                  routingKey: $"{station.Id}");
 
                 var consumer = new EventingBasicConsumer(channel);
+
                 consumer.Received += (model, ea) =>
                 {
                     var body = ea.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
                     var routingKey = ea.RoutingKey;
-                    Console.WriteLine(" [x] Received '{0}':'{1}'",
-                                      routingKey, message);
+                    Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);               
+                    var splittedMessage = message.Split(" ");
+
+                    station.Status = 1;
+                    int chargeValue = 0;
+
+                    for (; chargeValue <= Convert.ToInt32(splittedMessage[1]); chargeValue += 5) {
+
+                        var callbackMessage = Encoding.UTF8.GetBytes($"{splittedMessage[0]} {chargeValue}");
+                        channel.BasicPublish(exchange: "InboundExchange",
+                                     routingKey: "Charging",
+                                     basicProperties: null,
+                                     body: callbackMessage);
+                        Thread.Sleep(500);
+                    }
+                    station.Status = 0;
                 };
                 channel.BasicConsume(queue: $"{station.Id}",
                                      autoAck: true,
@@ -64,6 +85,21 @@ namespace StationConsumer
                 Console.ReadLine();
                 channel.QueueDelete($"{station.Id}");
             }
+        }
+        public static void Status(IConnection connection, Station station)
+        {
+            using (var channel = connection.CreateModel())
+            {                             
+                var body2 = Encoding.UTF8.GetBytes($"{station.Id} {station.Status}");
+                for (; ; )
+                {
+                    channel.BasicPublish(exchange: "InboundExchange",
+                                         routingKey: "Status",
+                                         basicProperties: null,
+                                         body: body2);
+                    Thread.Sleep(500);
+                }
+            }           
         }
     }
 }
